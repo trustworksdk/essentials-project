7a43b38943fe0ec1db5a1fc17932a22874de06f2
Jeppe Cramon <jeppe.cramon@trustworks.dk>
2025-08-01 08:01:41 +0200

    Added missing license header and corrected license formatting inconsistencies where applicable.

--- components/vaadin-ui/src/test/java/dk/trustworks/essentials/ui/task/TaskProcessor2.java
+++ components/vaadin-ui/src/test/java/dk/trustworks/essentials/ui/task/TaskProcessor2.java
@@ -1,38 +1,31 @@
-/*
+ /*
+ * Copyright 2021-2025 the original author or authors.
  *
- *  * Copyright 2021-2025 the original author or authors.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *      https://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package dk.trustworks.essentials.ui.task;
 
-import dk.trustworks.essentials.components.eventsourced.eventstore.postgresql.eventstream.AggregateType;
-import dk.trustworks.essentials.components.eventsourced.eventstore.postgresql.processor.EventProcessor;
-import dk.trustworks.essentials.components.eventsourced.eventstore.postgresql.processor.EventProcessorDependencies;
-import dk.trustworks.essentials.components.foundation.messaging.MessageHandler;
-import dk.trustworks.essentials.ui.task.domain.events.CommentAdded;
-import dk.trustworks.essentials.ui.task.domain.events.TaskCompleted;
-import dk.trustworks.essentials.ui.task.domain.events.TaskCreated;
-import dk.trustworks.essentials.ui.task.views.TaskView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+ import dk.trustworks.essentials.components.eventsourced.eventstore.postgresql.eventstream.AggregateType;
+ import dk.trustworks.essentials.components.eventsourced.eventstore.postgresql.processor.*;
+ import dk.trustworks.essentials.components.foundation.messaging.MessageHandler;
+ import dk.trustworks.essentials.ui.task.domain.events.*;
+ import dk.trustworks.essentials.ui.task.views.TaskView;
+ import org.slf4j.*;
+ import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+ import java.util.*;
+ import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class TaskProcessor2 extends EventProcessor {
